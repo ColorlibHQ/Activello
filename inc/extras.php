d460c9e5c22cc12d4e685e4217647ba1d15d5bb5
Free WP TP <freewptp@gmail.com>
2016-06-10 20:14:15 +0530

    Resolved the github issue #39

--- inc/extras.php
+++ inc/extras.php
@@ -68,7 +68,7 @@ function activello_custom_password_form() {
   $o = '<form class="protected-post-form" action="' . get_option('siteurl') . '/wp-login.php?action=postpass" method="post">
   <div class="row">
     <div class="col-lg-10">
-        ' . esc_html__( "<p>This post is password protected. To view it please enter your password below:</p>" ,'activello') . '
+        <p>' . esc_html__( "This post is password protected. To view it please enter your password below:" ,'activello') . '</p>
         <label for="' . $label . '">' . esc_html__( "Password:" ,'activello') . ' </label>
       <div class="input-group">
         <input class="form-control" value="' . get_search_query() . '" name="post_password" id="' . $label . '" type="password">
