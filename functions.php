6df824e181d025ee6b6070e85ee8842fa28ad1ec
Giucu <giucu91@yahoo.com>
2017-02-06 15:35:26 +0200

    #51 #52

--- functions.php
+++ functions.php
@@ -249,7 +249,7 @@ function activello_get_single_category($post_id){
     $post_categories = wp_get_post_categories( $post_id );
 
     if( !empty( $post_categories ) ){
-        return wp_list_categories('echo=0&title_li=&show_count=0&include='.$post_categories[0]);
+        return '<ul class="single-category">' . wp_list_categories('echo=0&title_li=&show_count=0&include='.$post_categories[0]) . '</ul>';
     }
     return '';
 }
