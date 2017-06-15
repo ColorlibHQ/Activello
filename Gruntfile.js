47a9a588af5477141123e258d65eaa38f1fe3210
Giucu <giucu91@yahoo.com>
2017-06-15 12:55:06 +0300

    Travis Auto Releases.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,50 @@ module.exports = function( grunt ) {
 
   grunt.config.init( {
     pkg: grunt.file.readJSON( 'package.json' ),
+
+    dirs: {
+      css: 'assets/css',
+      js: 'assets/js'
+    },
+
+    makepot: {
+      target: {
+        options: {
+          domainPath: '/languages/',
+          potFilename: '<%= pkg.name %>.pot',
+          potHeaders: {
+            poedit: true,
+            'x-poedit-keywordslist': true
+          },
+          processPot: function( pot, options ) {
+            pot.headers[ 'report-msgid-bugs-to' ] = 'https://www.colorlib.com/';
+            pot.headers[ 'language-team' ] = 'Colorlib <office@colorlib.com>';
+            pot.headers[ 'last-translator' ] = 'Colorlib <office@colorlib.com>';
+            pot.headers[ 'language-team' ] = 'Colorlib <office@colorlib.com>';
+            return pot;
+          },
+          updateTimestamp: true,
+          type: 'wp-theme'
+
+        }
+      }
+    },
+
+    addtextdomain: {
+      target: {
+        options: {
+          updateDomains: true,
+          textdomain: '<%= pkg.name %>'
+        },
+        files: {
+          src: [
+            '*.php',
+            '!node_modules/**'
+          ]
+        }
+      }
+    },
+
     checktextdomain: {
       standard: {
         options: {
@@ -33,12 +77,84 @@ module.exports = function( grunt ) {
             src: [
               '**/*.php',
               '!**/node_modules/**',
-              '!**/vendor/**',
             ], //all php
             expand: true
           } ]
       }
-    }
+    },
+
+    clean: {
+      init: {
+        src: [ 'build/' ]
+      },
+      build: {
+        src: [
+          'build/*',
+          '!build/<%= pkg.name %>.zip'
+        ]
+      },
+    },
+
+    copy: {
+        readme: {
+            src: 'readme.md',
+            dest: 'build/readme.txt'
+        },
+      build: {
+        expand: true,
+        src: [
+          '**',
+          '!node_modules/**',
+          '!vendor/**',
+          '!build/**',
+          '!readme.md',
+          '!README.md',
+          '!phpcs.ruleset.xml',
+          '!Gruntfile.js',
+          '!package.json',
+          '!composer.json',
+          '!composer.lock',
+          '!set_tags.sh',
+          '!activello.zip',
+          '!nbproject/**' ],
+        dest: 'build/'
+      }
+    },
+
+    compress: {
+      build: {
+        options: {
+          pretty: true,
+          archive: '<%= pkg.name %>.zip'
+        },
+        expand: true,
+        cwd: 'build/',
+        src: [ '**/*' ],
+        dest: '<%= pkg.name %>/'
+      }
+    },
+
+    imagemin: {
+      jpg: {
+        options: {
+          progressive: true
+        }
+      },
+      png: {
+        options: {
+          optimizationLevel: 7
+        }
+      },
+      dynamic: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/img/',
+            src: [ '**/*.{png,jpg,gif}' ],
+            dest: 'assets/img/'
+          } ]
+      }
+    },
 
   } );
 
@@ -47,4 +163,16 @@ module.exports = function( grunt ) {
     'checktextdomain'
   ] );
 
+  // Minify Images
+  grunt.registerTask( 'minimg', [
+    'imagemin:dynamic'
+  ] );
+
+  // Build task
+  grunt.registerTask( 'build-archive', [
+    'clean:init',
+    'copy',
+    'compress:build',
+    'clean:init'
+  ] );
 };
